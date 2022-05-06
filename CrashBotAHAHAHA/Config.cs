05679fa056c8101c1eb58a30d804fa948290050d
EzCq <97411267+EzCq@users.noreply.github.com>
2022-05-06 14:41:27 +0300

    Add files via upload

---
+++ CrashBotAHAHAHA/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrashBotAHAHAHA
+{
+    public class Config
+    {
+        // By cringel 
+
+
+        public static string server_url = "https://discord.gg/fzlgroup2"; // ссылка на ваш дискорд сервер
+        public static string telegram_url = "https://t.me/protectcheck"; // ссылка на ваш тг канал
+        public static string youtube_url = "https://www.youtube.com/c/forzeldiscord"; // kek
+    }
+}
---
+++ CrashBotAHAHAHA/logging/logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrashBotAHAHAHA.logging
+{
+    public class logger
+    {
+        public static void succesLog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+        public static void errorLog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+    }
+}
